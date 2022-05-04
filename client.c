6530f804b944170a200f73824e2152aa7e98c317
Hogwon Kim <hogkim@c4r10s7.42seoul.kr>
2022-05-04 20:10:13 +0900

    1

--- client.c
+++ client.c
@@ -6,20 +6,20 @@
 /*   By: hogkim <hogkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 12:39:48 by hogkim            #+#    #+#             */
-/*   Updated: 2022/05/04 19:16:04 by hogkim           ###   ########.fr       */
+/*   Updated: 2022/05/04 20:09:58 by hogkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft/libft.h"
 
-void	ft_print_to_server(int to_server)
+/* void	ft_print_to_server(int len)
 {
-	ft_putstr_fd("To server    : ", 1);
-	ft_putnbr_fd(to_server, 1);
+	ft_putstr_fd("To server   : ", 1);
+	ft_putnbr_fd(len, 1);
 	ft_putchar_fd('\n', 1);
 }
-
+ */
 void	ft_confirmed(int signo)
 {
 	static int	from_server = 0;
@@ -28,41 +28,38 @@ void	ft_confirmed(int signo)
 		from_server++;
 	if (signo == SIGUSR1)
 	{
-		ft_putstr_fd("From Server :", 1);
+/* 		ft_putstr_fd("From Server : ", 1);
 		ft_putnbr_fd(from_server, 1);
-		ft_putchar_fd('\n', 1);
+		ft_putchar_fd('\n', 1); */
+		ft_printf("From Server : %d\n", from_server);
 		exit(0);
 	}
 }
 
 void	ft_to_server(pid_t server_pid, char *str)
 {
-	int	i;
 	int	j;
-	int	to_server;
+	char c;
 
-	to_server = 0;
-	i = -1;
-	while (str[++i])
+	while (*str)
 	{
 		j = 8;
+		c = *str++;
 		while (j--)
 		{
-			if ((str[i] >> j) & 1)
+			if ((c >> j) & 1)
 				kill(server_pid, SIGUSR2);
 			else
 				kill(server_pid, SIGUSR1);
-			usleep(1000);
+			usleep(80);
 		}
-		to_server++;
 	}
-	i = 8;
-	while (i--)
+	j = 8;
+	while (j--)
 	{
 		kill(server_pid, SIGUSR1);
-		usleep(1000);
+		usleep(80);
 	}
-	ft_print_to_server(to_server);
 }
 
 int	main(int argc, char **argv)
@@ -72,6 +69,8 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("arguments are wrong\n", 1);
 		return (0);
 	}
+	ft_printf("To Server   : %d\n", ft_strlen(argv[2]))''
+/* 	ft_print_to_server(ft_strlen(argv[2])); */
 	signal(SIGUSR1, ft_confirmed);
 	signal(SIGUSR2, ft_confirmed);	
 	ft_to_server(ft_atoi(argv[1]), argv[2]);
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: hogkim <hogkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 12:39:53 by hogkim            #+#    #+#             */
-/*   Updated: 2022/05/04 19:16:39 by hogkim           ###   ########.fr       */
+/*   Updated: 2022/05/04 20:07:51 by hogkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,60 +17,39 @@ void	ft_from_client(int signo, siginfo_t *si, void *context)
 {
 	static int		i = 0;
 	static char		c = 0;
-	static pid_t	client_pid = 0;
 
-	client_pid = si->si_pid;
 	if (signo == SIGUSR2)
 		c |= 1;
-	ft_putnbr_fd(i, 1);
 	if (++i == 8)
 	{
+		i = 0;
 		if (!c)
 		{
-			kill(client_pid, SIGUSR1);
+			kill(si->si_pid, SIGUSR1);
 			return ;
 		}
-		ft_putchar_fd(c, 1);
-		kill(client_pid, SIGUSR2);
-		i = 0;
+		/* ft_putchar_fd(c, 1); */
+		ft_printf("%c", c);
 		c = 0;
+		kill(si->si_pid, SIGUSR2);
 	}
 	else
 		c <<= 1;
 	(void)context;
 }
 
-void	ft_from_client2(int signo)
-{
-	static int	i = 0;
-	static char	c = 0;
-
-	if (signo == SIGUSR2)
-		c |= 1;
-	if (++i == 8)
-	{
-		ft_putchar_fd(c, 1);
-		i = 0;
-		c = 0;
-	}
-	else
-		c <<= 1;
-}
-
 int	main(void)
 {
 	struct sigaction	sa;
 
-	ft_putstr_fd("Server PID : ", 1);
+/* 	ft_putstr_fd("Server PID : ", 1);
 	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd('\n', 1);
-/* 	sa.sa_flags = SA_SIGINFO; */
+	ft_putchar_fd('\n', 1); */
+	ft_printf("Server PID : %u\n", getpid());
+	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = ft_from_client;
-	sa.sa_handler = ft_from_client2;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-/* 	signal(SIGUSR1, ft_from_client2);
-	signal(SIGUSR2, ft_from_client2); */
 	while (1)
 		pause();
 	return (0);
